6a8a3980da9f32e8c270cb98ca343cc3590d4c63
agustinrobledo <agustinrobledo59.ar@gmail.com>
2023-04-23 19:27:25 -0300

    added classname in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,15 @@
 interface buttonProps {
-  filled?: boolean,
-  color?: string,
+  filled?: boolean
+  color?: string
   children: React.ReactNode
+  className: string
 }
 
-const Button = ({ filled, color = 'black', children }: buttonProps) => {
+const Button = ({ filled, color = 'black', children, className }: buttonProps) => {
 
   return (
     <button
-      className={`border-2 border-black rounded-md ${filled ? `bg-${color}` : 'bg-transparent'}`}
+      className={`border-2 border-black rounded-md ${filled ? `bg-${color}` : 'bg-transparent'} ${className}`}
     >{children}</button>
   )
 }
