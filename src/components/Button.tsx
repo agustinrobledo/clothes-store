e39c4543062646b318276632e514272b97112d52
agustinrobledo <agustinrobledo59.ar@gmail.com>
2023-04-23 20:51:39 -0300

    hover and search

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,15 @@ interface buttonProps {
   filled?: boolean
   color?: string
   children: React.ReactNode
-  className: string
+  border?: boolean
+  className?: string
 }
 
-const Button = ({ filled, color = 'black', children, className }: buttonProps) => {
+const Button = ({ filled, color = 'black', children, className, border = true }: buttonProps) => {
 
   return (
     <button
-      className={`border-2 border-black rounded-md ${filled ? `bg-${color}` : 'bg-transparent'} ${className}`}
+      className={`${border ? "border-2 border-black" : ""} px-4 rounded-xl ${filled ? `bg-${color}` : 'bg-transparent'} duration-100 hover:bg-black hover:text-white ${className}`}
     >{children}</button>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,17 @@ import Button from "./Button"
 
 const Navbar = () => {
   return (
-    <div className="bg-white px-12 py-6 text-black justify-between w-100 flex">
-      <div className="" >
+    <div className="bg-white px-12 py-6 font-semibold text-black justify-between w-100 flex">
+      <div className="flex gap-2" >
         <Button>Shop</Button>
         <Button>About</Button>
+        <Button border={false}>🔎</Button>
       </div>
-      <h1>THE FIGMA STORE</h1>
-      <div>
+      <h1 className="font-bold text-xl">THE FIGMA STORE</h1>
+      <div className="flex gap-2">
         {/* // this is going to be a dropdown */}
         <Button>UNITED STATES</Button>
-        <Button>CART</Button>
+        <Button>Cart</Button>
       </div>
     </div>
   )
