9105ae9844e99173109e79f728cfe3fb9da904cf
agustinrobledo <agustinrobledo59.ar@gmail.com>
2023-04-24 22:25:51 -0300

    added navbar styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,29 @@
 interface buttonProps {
-  filled?: boolean
-  color?: string
-  children: React.ReactNode
-  border?: boolean
-  className?: string
+    filled?: boolean
+    color?: string
+    children: React.ReactNode
+    border?: boolean
+    className?: string
 }
 
-const Button = ({ filled, color = 'black', children, className, border = true }: buttonProps) => {
-
-  return (
-    <button
-      className={`${border ? "border-2 border-black" : ""} px-4 rounded-xl ${filled ? `bg-${color}` : 'bg-transparent'} duration-100 hover:bg-black hover:text-white ${className}`}
-    >{children}</button>
-  )
+const Button = ({
+    filled,
+    color = "black",
+    children,
+    className,
+    border = true,
+}: buttonProps) => {
+    return (
+        <button
+            className={`${
+                border ? "border-2 border-black" : ""
+            } px-4 rounded-xl ${
+                filled ? `bg-${color}` : "bg-transparent"
+            } duration-300 hover:bg-black hover:text-white ${className}`}
+        >
+            {children}
+        </button>
+    )
 }
 
 export default Button
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,21 @@
 import Button from "./Button"
 
 const Navbar = () => {
-  return (
-    <div className="bg-white px-12 py-6 font-semibold text-black justify-between w-100 flex">
-      <div className="flex gap-2" >
-        <Button>Shop</Button>
-        <Button>About</Button>
-        <Button border={false}>🔎</Button>
-      </div>
-      <h1 className="font-bold text-xl">THE FIGMA STORE</h1>
-      <div className="flex gap-2">
-        {/* // this is going to be a dropdown */}
-        <Button>UNITED STATES</Button>
-        <Button>Cart</Button>
-      </div>
-    </div>
-  )
+    return (
+        <div className="bg-yellow-500 px-12 py-6 font-semibold text-black justify-between w-100 flex">
+            <div className="flex gap-2">
+                <Button>SHOP</Button>
+                <Button>ABOUT</Button>
+                <Button border={false}>🔎</Button>
+            </div>
+            <h1 className="font-bold text-xl">THE FIGMA STORE</h1>
+            <div className="flex gap-2">
+                {/* // this is going to be a dropdown */}
+                <Button>UNITED STATES</Button>
+                <Button>CART</Button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
